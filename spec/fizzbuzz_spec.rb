fe1175d09ef57dbec97c70ad14e1fdb764408270
vikipretium <vighneshpasupathy@gmail.com>
2022-05-27 21:30:14 +0530

    add test for non divisible numbers

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -13,5 +13,8 @@ describe Solver do
       it 'If N is divisible by 3 and 5, return fizzbuzz' do
         expect(solver.fizzbuzz(30)).to eql 'fizzbuzz'
       end
+      it 'If N is non-divisible by 3 and 5, return number as string' do
+        expect(solver.fizzbuzz(2)).to eql '2'
+      end
   end
 end
