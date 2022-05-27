3af4ca974992adcda28237c3ed5656be87daf644
vikipretium <vighneshpasupathy@gmail.com>
2022-05-27 20:33:27 +0530

    add factorial test for 3 different conditions

---
+++ spec/factorial_spec.rb
@@ -0,0 +1,17 @@
+require_relative '../solver'
+
+describe Solver do
+  context 'testing the solver class' do
+    solver = Solver.new
+
+    it 'method arise an error if number is zero' do
+      expect(solver.factorial(0)).to eql 1
+    end
+    it 'method arise an error if number is positive' do
+      expect(solver.factorial(4)).to eql 24
+    end
+    it 'method arise an error if number is negative' do
+      expect { solver.factorial(-6) }.to raise_error('Please enter positive number')
+    end
+  end
+end
